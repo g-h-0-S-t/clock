0558b91fa76e773578517d780e69dba36fb85570
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2022-07-11 06:50:30 +0530

    font-size fix

--- clock.js
+++ clock.js
@@ -14,10 +14,10 @@ javascript: ((window) => {
         "a {color: #0a95ff !important;}" +
         "#clock {display: flex !important; align-items: center !important; justify-content: center !important; position: fixed !important; width: 100% !important; height: 100% !important; text-align: center !important; margin: 0 auto !important;}" +
         "#clockTable {font-size: 12vw !important;}" +
-        "#time {text-align: center !important; font-size: 1.5em !important;}" +
+        "#time {text-align: center !important; font-size: 2em !important;}" +
         "#seconds {display: inline-block !important; width: 1.12em !important; font-size: 0.14em !important; text-align: left !important;}" +
-        "#meridian {display: inline-block !important; width: fit-content !important; font-size: 1em !important; text-align: right !important;}" +
-        "#timeDivider {display: inline-block !important; width: 0.27em !important; font-size: 1em !important;}" +
+        "#meridian {display: inline-block !important; width: fit-content !important; font-size: 0.5em !important; text-align: right !important;}" +
+        "#timeDivider {display: inline-block !important; width: 0.27em !important; font-size: 0.5em !important;}" +
         "#date {text-align: center !important; font-size: 0.5em !important;}" +
         "#devMsg {text-align: center !important; font-weight: bold !important; font-size: 2rem !important; position: fixed !important; top: 1rem !important; left: 0 !important; right: 0 !important; z-index: 1 !important;}";
     window.document.head.appendChild(clockCSS);
